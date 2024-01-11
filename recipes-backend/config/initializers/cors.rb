8bd252fb76eb687e9fbd2f415a596af89da5ed51
davidwter <dweinachter@sweep.net>
2024-01-11 16:09:33 +0100

    modifying CORS and removing PG files

--- recipes-backend/config/initializers/cors.rb
+++ recipes-backend/config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'  # or use '*' to allow all origins
+      origins '*'  # or use '*' to allow all origins
   
       resource '*',
         headers: :any,
