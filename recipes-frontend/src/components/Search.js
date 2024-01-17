8b081d6e4239fbfae8268a798a5ab0dacd2339bd
davidwter <dweinachter@sweep.net>
2024-01-17 22:35:53 +0100

    Amélioration de la pagination

--- recipes-frontend/src/components/Search.js
+++ recipes-frontend/src/components/Search.js
@@ -11,6 +11,8 @@ import {
   CircularProgress,
   Pagination,
 } from "@mui/material";
+import { useNavigate, useLocation } from 'react-router-dom';
+
 
 import axios from "axios";
 
@@ -19,12 +21,17 @@ const apiBaseUrl =
 
 const Search = ({ onSearch, searchQuery, setSearchQuery }) => {
   const [isSearching, setIsSearching] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
+  //const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
 
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState("");
   const [snackbarSeverity, setSnackbarSeverity] = useState("info");
+
+  const navigate = useNavigate();
+  const location = useLocation(); 
+  const queryParams = new URLSearchParams(location.search);
+  const currentPage = parseInt(queryParams.get('page') || 1, 10);
 
   const handleSnackbarClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -39,11 +46,13 @@ const Search = ({ onSearch, searchQuery, setSearchQuery }) => {
     setOpenSnackbar(true);
   };
 
-  const handleSearch = async (page = 1) => {
+  const handleSearch = async (page = 1, isNewSearch = false) => {
     if (!searchQuery.trim()) {
       showAlert("Merci de saisir quelques ingrédients", "warning");
       return;
     }
+
+    
 
     setIsSearching(true); // Start the loading indicator
 
@@ -53,8 +62,13 @@ const Search = ({ onSearch, searchQuery, setSearchQuery }) => {
       });
       onSearch(response.data.recipes);
       setTotalPages(response.data.total_pages);
-      setCurrentPage(response.data.current_page);
+      //setCurrentPage(response.data.current_page);
       setIsSearching(false); // Stop the loading indicator once data is fetched
+      if (isNewSearch) {
+        navigate(`?page=1`);
+    }
+
+
     } catch (error) {
       console.error("Error fetching recipes", error);
       showAlert(
@@ -68,12 +82,14 @@ const Search = ({ onSearch, searchQuery, setSearchQuery }) => {
   const handleClear = () => {
     setSearchQuery(""); // Clear the search query
     onSearch([]); // Clear the search results
+    navigate(`?page=1`); // Reset the page in the URL
+
   };
 
   const handlePageChange = (event, page) => {
-    setCurrentPage(page);
-    handleSearch(page);
-  };
+      navigate(`?page=${page}`);
+      handleSearch(page);
+    }
 
   return (
     <Paper sx={{ padding: "2rem", margin: "2rem" }}>
@@ -96,7 +112,7 @@ const Search = ({ onSearch, searchQuery, setSearchQuery }) => {
           <Button
             variant="contained"
             color="primary"
-            onClick={() => handleSearch(currentPage)}
+            onClick={() => handleSearch(1, true)}
             sx={{ marginRight: "1rem" }}
           >
             Rechercher
--- recipes-frontend/src/components/SearchPage.js
+++ recipes-frontend/src/components/SearchPage.js
@@ -1,12 +1,23 @@
 // src/components/SearchPage.js
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
 import Search from './Search';
 import RecipeList from './RecipeList';
 
 const SearchPage = ({ recipes, setRecipes, searchQuery, setSearchQuery }) => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const currentPage = parseInt(queryParams.get('page') || 1, 10);
+
   return (
     <div>
-      <Search onSearch={setRecipes} searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <Search 
+      onSearch={setRecipes} 
+      searchQuery={searchQuery} 
+      setSearchQuery={setSearchQuery} 
+      currentPage= {currentPage}
+      />
       <RecipeList recipes={recipes} />
     </div>
   );
